756f256642a623559c363849594216f9d4b01379
ZakiTech-cmd <103463235+ZakiTech-cmd@users.noreply.github.com>
2023-12-07 17:59:43 +0200

    Creating Fighter Model

--- fight_club_app/models.py
+++ fight_club_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Fighter(models.Model):
+    picture = models.ImageField(null=True, blank=True, verbose_name='Imagine')
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    nation = models.CharField(max_length=50)
+    weight = models.IntegerField(null=True)
+    height = models.IntegerField(null=True)
+    age = models.IntegerField(null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
