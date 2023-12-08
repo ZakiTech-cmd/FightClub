8950fdc13f2866a3cea85859fc71e34d308036f7
ZakiTech-cmd <103463235+ZakiTech-cmd@users.noreply.github.com>
2023-12-08 15:32:24 +0200

    Add gitignore and remove fightclub app

--- fight_club/settings.py
+++ fight_club/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fight_club_app',
 ]
 
 MIDDLEWARE = [
--- fight_club_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FightClubAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fight_club_app'
