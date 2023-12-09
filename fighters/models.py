7ff39161c4232f829ba6c1ae7329eb861aedef87
ZakiTech-cmd <103463235+ZakiTech-cmd@users.noreply.github.com>
2023-12-09 16:21:38 +0200

    Adding title serilizer and making some changes in models. (#9)

--- fighters/models.py
+++ fighters/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 
 # Create your models here.
+class Title(models.Model):
+    name = models.CharField()
+
+    def __str__(self):
+        return self.name
 
 
 class Fighter(models.Model):
     picture = models.ImageField(null=True)
+    title = models.ForeignKey(Title, default=None, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     nation = models.CharField(max_length=50)
@@ -15,15 +21,6 @@ class Fighter(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-
-
-class Title(models.Model):
-    fighter = models.ForeignKey(Fighter, default=None, on_delete=models.CASCADE)
-    name = models.CharField()
-    date = models.DateField()
-
-    def __str__(self):
-        return self.name
 
 
 class Match(models.Model):
