5ff175f3b573483329e568c0860898734c5101f0
ZakiTech-cmd <103463235+ZakiTech-cmd@users.noreply.github.com>
2023-12-09 16:59:09 +0200

    adding view functions for Match and titles (#11)

--- fighters/views.py
+++ fighters/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import generics
-from .models import Fighter
-from .serializers import FighterSerializer
+from .models import Fighter, Match, Title
+from .serializers import FighterSerializer, MatchSerializer, TitleSerializer
 
 
 class FighterListCreateView(generics.ListCreateAPIView):
@@ -14,3 +14,23 @@ class FighterListCreateView(generics.ListCreateAPIView):
 class FighterRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Fighter.objects.all()
     serializer_class = FighterSerializer
+
+
+class TitleListCreateView(generics.ListCreateAPIView):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+
+
+class TitleRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+
+
+class MatchListCreateView(generics.ListCreateAPIView):
+    queryset = Match.objects.all()
+    serializer_class = MatchSerializer
+
+
+class MatchRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Match.objects.all()
+    serializer_class = MatchSerializer
