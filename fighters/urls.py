74b4622f43a0f813cf70cfde08af362e3f24ca9b
alexandrucurcubat <alexandrucurcubat@gmail.com>
2023-12-10 12:43:30 +0200

    Fix urls and allow CORS

--- fighters/urls.py
+++ fighters/urls.py
@@ -4,10 +4,10 @@ from .views import FighterListCreateView, FighterRetrieveUpdateDestroyView, Titl
     TitleRetrieveUpdateDestroyView, MatchListCreateView, MatchRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path('/fighters', FighterListCreateView.as_view(), name='fighter-list-create'),
-    path('/fighters/<int:pk>', FighterRetrieveUpdateDestroyView.as_view(), name='fighter-retrieve-update-destroy'),
-    path('/titles', TitleListCreateView.as_view(), name='title-list-create'),
-    path('/titles/<int:pk>', TitleRetrieveUpdateDestroyView.as_view(), name='title-retrieve-update-destroy'),
-    path('/matches', MatchListCreateView.as_view(), name='match-list-create'),
-    path('/matches/<int:pk>', MatchRetrieveUpdateDestroyView.as_view(), name='match-retrieve-update-destroy'),
+    path('fighters', FighterListCreateView.as_view(), name='fighter-list-create'),
+    path('fighters/<int:pk>', FighterRetrieveUpdateDestroyView.as_view(), name='fighter-retrieve-update-destroy'),
+    path('titles', TitleListCreateView.as_view(), name='title-list-create'),
+    path('titles/<int:pk>', TitleRetrieveUpdateDestroyView.as_view(), name='title-retrieve-update-destroy'),
+    path('matches', MatchListCreateView.as_view(), name='match-list-create'),
+    path('matches/<int:pk>', MatchRetrieveUpdateDestroyView.as_view(), name='match-retrieve-update-destroy'),
 ]
