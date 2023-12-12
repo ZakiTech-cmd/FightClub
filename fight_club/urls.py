1242fcc007245a96ff58402dfac71ecd2c6c2e01
ZakiTech-cmd <103463235+ZakiTech-cmd@users.noreply.github.com>
2023-12-12 14:47:52 +0200

    Adding match functionality (#14)

--- fight_club/urls.py
+++ fight_club/urls.py
@@ -3,14 +3,15 @@ from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
 from fighters.views import FighterViewSet
+from matches.views import MatchViewSet
 from titles.views import TitleViewSet
 
 router = SimpleRouter()
 router.register('titles', TitleViewSet)
 router.register('fighters', FighterViewSet)
+router.register('matches', MatchViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('matches/', include('matches.urls')),
     path('users/', include('users.urls')),
 ] + router.urls
