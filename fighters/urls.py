dc16b9770633c5e3469e9df59ccfd6f7f53aabc2
ZakiTech-cmd <103463235+ZakiTech-cmd@users.noreply.github.com>
2023-12-12 14:26:32 +0200

    Adding the fighter setviews (#13)

--- fighters/urls.py
+++
@@ -1,8 +0,0 @@
-# fighters/urls.py
-from django.urls import path
-from .views import FighterListCreateView, FighterRetrieveUpdateDestroyView
-
-urlpatterns = [
-    path('', FighterListCreateView.as_view(), name='fighter-list-create'),
-    path('<int:pk>', FighterRetrieveUpdateDestroyView.as_view(), name='fighter-retrieve-update-destroy')
-]
