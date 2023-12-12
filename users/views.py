f50fbb37622f17ff7662983ea6d90411eefc5f56
ZakiTech-cmd <103463235+ZakiTech-cmd@users.noreply.github.com>
2023-12-12 14:50:27 +0200

    Users update

--- users/views.py
+++ users/views.py
@@ -1,6 +1,5 @@
-from rest_framework.authtoken.models import Token
 from rest_framework import generics, permissions
-from rest_framework.authtoken.views import ObtainAuthToken
+
 from .models import User
 from .serializers import UserSerializer
 from rest_framework.views import APIView
@@ -23,13 +22,3 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.AllowAny]
-
-
-class CustomObtainAuthToken(ObtainAuthToken):
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data,
-                                           context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key})
