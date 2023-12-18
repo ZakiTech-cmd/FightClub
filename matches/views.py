1bcdce2d4b7f09bd0f0d10fd8ab1f8fd304a2a32
ZakiTech-cmd <103463235+ZakiTech-cmd@users.noreply.github.com>
2023-12-18 16:11:39 +0200

    Add filter functionality for owner and fighters in matches

--- matches/views.py
+++ matches/views.py
@@ -1,3 +1,6 @@
+from django.db.models import Q
+from django_filters import rest_framework as filters
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 
 from utils.permissions import IsAdminOrReadOnly
@@ -5,8 +8,24 @@ from .models import Match
 from .serializers import MatchSerializer
 
 
-# Create your views here.
+class MatchFilter(filters.FilterSet):
+    owner = filters.NumberFilter(method="owner_filter")
+    fighter = filters.NumberFilter(method="fighter_filter")
+
+    class Meta:
+        model = Match
+        fields = ['owner', 'fighter']
+
+    def owner_filter(self, queryset, name, value):
+        return queryset.filter(Q(challenger__owner=value) | Q(defender__owner=value))
+
+    def fighter_filter(self, queryset, name, value):
+        return queryset.filter(Q(challenger=value) | Q(defender=value))
+
+
 class MatchViewSet(ModelViewSet):
     queryset = Match.objects.all()
     serializer_class = MatchSerializer
     permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = MatchFilter
