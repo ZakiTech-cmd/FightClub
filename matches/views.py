9fec0b12e169c5827f3833fbcaf4f615b406fc73
ZakiTech-cmd <103463235+ZakiTech-cmd@users.noreply.github.com>
2023-12-18 16:43:01 +0200

    Adding random match functionality

--- matches/views.py
+++ matches/views.py
@@ -1,8 +1,15 @@
+import random
+from datetime import date, timedelta
+
 from django.db.models import Q
 from django_filters import rest_framework as filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
+from fighters.models import Fighter
 from utils.permissions import IsAdminOrReadOnly
 from .models import Match
 from .serializers import MatchSerializer
@@ -29,3 +36,16 @@ class MatchViewSet(ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
     filter_backends = [DjangoFilterBackend]
     filterset_class = MatchFilter
+
+    @action(methods=["POST"], detail=False, url_path="random")
+    def random_match(self, request):
+        if Fighter.objects.count() < 2:
+            raise ValidationError("You don't have enough fighters.")
+
+        challenger = Fighter.objects.order_by("?").first()
+        defender = Fighter.objects.exclude(id=challenger.id).order_by("?").first()
+        random_days = random.randint(1, 30)
+        future_date = date.today() + timedelta(days=random_days)
+        match = Match.objects.create(challenger=challenger, defender=defender, date=future_date)
+
+        return Response(MatchSerializer(match).data)
