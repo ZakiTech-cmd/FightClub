119e4dda39e3daf4eda4b640ec7e251ae2d7af29
ZakiTech-cmd <103463235+ZakiTech-cmd@users.noreply.github.com>
2023-12-18 15:38:37 +0200

    Adding validation functionality for matches

--- matches/serializers.py
+++ matches/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from rest_framework.exceptions import ValidationError
 from matches.models import Match
 
 
@@ -7,3 +7,22 @@ class MatchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Match
         fields = '__all__'
+
+    def validate(self, attrs):
+        challenger = None
+        defender = None
+
+        if self.instance:
+            challenger = self.instance.challenger
+            defender = self.instance.defender
+
+        if "challenger" in attrs:
+            challenger = attrs['challenger']
+
+        if "defender" in attrs:
+            defender = attrs['defender']
+
+        if challenger == defender:
+            raise ValidationError("Challenger must be different from Defender")
+
+        return attrs
