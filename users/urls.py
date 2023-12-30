2b74a4fc500b143075e41e4ecf83ed2f77475722
ZakiTech-cmd <103463235+ZakiTech-cmd@users.noreply.github.com>
2023-12-31 00:05:01 +0200

    Adding view for logged user information

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path
-from .views import RegisterView, LogoutView
+from .views import RegisterView, LogoutView, UserDetailView
 
 urlpatterns = [
     path('logout', LogoutView.as_view(), name='logout'),
     path('register', RegisterView.as_view(), name='register'),
     path('login', obtain_auth_token, name='login'),
+    path('my-profile/', UserDetailView.as_view(), name='my-profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -22,3 +22,11 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.AllowAny]
+
+
+class UserDetailView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
