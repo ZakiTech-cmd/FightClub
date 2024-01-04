7c39fc5cc7263ad5e0dacdec5a0e3e1bc3d01ff2
alexandrucurcubat <alexandrucurcubat@gmail.com>
2024-01-04 15:04:46 +0200

    Remove club_name from User serializer

--- users/serializers.py
+++ users/serializers.py
@@ -6,7 +6,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'first_name', 'last_name', 'club_name']
+        fields = ['id', 'username', 'password', 'first_name', 'last_name']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
